01b3851c94acf600630ebf37136aa2fc10c9a0bb
Hien Luong <hyhien@gmail.com>
2023-06-17 09:40:34 +0700

    Init project

---
+++ Lab01Assignment/Lab01ViDu/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Lab01ViDu.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
