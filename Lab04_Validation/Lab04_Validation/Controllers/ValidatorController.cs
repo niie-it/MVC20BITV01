0133ad063374f191610965c3d4f8318b6ddced4f
Hien Luong <hyhien@gmail.com>
2023-07-08 08:02:20 +0700

    Demo simple validation

---
+++ Lab04_Validation/Lab04_Validation/Controllers/ValidatorController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Lab04_Validation.Controllers
+{
+    public class ValidatorController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Validate()
+        {
+            if (ModelState.IsValid)
+            {
+                ViewBag.Message = "Dữ liệu hợp lệ";
+                //TODO: Do business
+            }
+            else
+            {
+                ModelState.AddModelError("loi", "Còn lỗi");
+            }
+            return View("Index");
+        }
+    }
+}
---
+++ Lab04_Validation/Lab04_Validation/Models/EmployeeInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Lab04_Validation.Models
+{
+    public class EmployeeInfo
+    {
+        [Display(Name ="Họ tên")]
+        [MinLength(5, ErrorMessage ="Tối thiểu 5 kí tự")]
+        public string FullName { get; set; }
+
+        [Display(Name ="Tuổi")]
+        [Range(18, 65, ErrorMessage ="Tuổi từ 18 --> 65")]
+        public int Age { get; set; }
+    }
+}
